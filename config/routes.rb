a681dcd61638633fb8081e5c3ebef845c818c4df
oxanayoxana <oksana.oleshchenko@gmail.com>
2018-10-06 20:12:59 +0300

    Finish the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'
 
   resources :users
 end
